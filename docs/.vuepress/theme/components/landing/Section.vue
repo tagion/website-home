284f9058ecb5654f523de68b4b42c973e381ca30
Vladislav Pazych <vladpazych@gmail.com>
2020-02-05 12:53:59 +0200

    Split to more components

--- docs/.vuepress/theme/components/landing/Section.vue
+++ docs/.vuepress/theme/components/landing/Section.vue
@@ -1,5 +1,12 @@
 <template>
-  <div class="section"></div>
+  <div class="section">
+    <div class="section__background">
+      <slot name="background" />
+    </div>
+    <div class="section__container">
+      <slot name="container" />
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,3 +14,13 @@ export default {
   props: {}
 };
 </script>
+
+<style lang="scss" scoped>
+.section {
+  .section__backgorund {
+  }
+
+  .section__container {
+  }
+}
+</style>
---
+++ docs/.vuepress/theme/components/landing/sections/Hero.vue
@@ -0,0 +1,21 @@
+<template>
+  <section class="section">
+    
+  </section>
+</template>
+
+<script>
+import Section from "../Section";
+
+export default {
+  props: {},
+  components: {
+    Section
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.section {
+}
+</style>
