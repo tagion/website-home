97f31ad380a66fc906a43ce1aad08c8ad4bd2017
Vladislav Pazych <vladpazych@gmail.com>
2020-03-29 11:38:54 +0300

    Return whitepaper

--- docs/.vuepress/config/navigation.js
+++ docs/.vuepress/config/navigation.js
@@ -3,7 +3,7 @@ module.exports = {
   nav: [
     { text: "Tagion", link: "/" },
     { text: "", link: "sep1" },
-    // { text: "Whitepaper", link: "https://github.com/tagion/resources/raw/master/whitepaper/tagion-whitepaper.pdf" },
+    { text: "Whitepaper", link: "https://github.com/tagion/resources/raw/master/whitepaper/tagion-whitepaper.pdf" },
     { text: "Technical Paper", link: "https://github.com/tagion/resources/raw/master/technical-paper/tagion-technical-paper.pdf" },
     { text: "", link: "sep2" },
     { text: "GitHub", link: "https://github.com/tagion" },
