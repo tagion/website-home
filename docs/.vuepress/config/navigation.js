4fa523e8be1bbb768934c0fd405a51420a653448
Vladislav Pazych <vladpazych@gmail.com>
2020-05-07 10:35:28 +0300

    Add FAQ link

--- docs/.vuepress/config/navigation.js
+++ docs/.vuepress/config/navigation.js
@@ -3,6 +3,7 @@ module.exports = {
   nav: [
     { text: "Tagion", link: "/" },
     { text: "", link: "sep1" },
+    { text: "FAQ", link: "https://forum.tagion.org/t/tagion-faq/" },
     { text: "Whitepaper", link: "https://github.com/tagion/resources/raw/master/whitepaper/tagion-whitepaper.pdf" },
     { text: "Technical Paper", link: "https://github.com/tagion/resources/raw/master/technical-paper/tagion-technical-paper.pdf" },
     { text: "", link: "sep2" },
